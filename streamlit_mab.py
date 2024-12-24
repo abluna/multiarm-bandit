f9c84bf160068c92aa95330fce4236f747772192
Abraham <baldenegro40@gmail.com>
2024-12-24 12:28:21 -0800

    Update streamlit_mab.py

--- streamlit_mab.py
+++ streamlit_mab.py
@@ -19,6 +19,8 @@ warnings.simplefilter("ignore", category=FutureWarning)
 pd.set_option('display.max_columns', None)
 
 st.markdown("<h2 style='text-align: center; color: grey;'>Multi-Armed Bandit Campaign Simulator</h2>", unsafe_allow_html=True)
+st.markdown("<h4 style='text-align: justify; color: grey;'>placerholder</h4>", unsafe_allow_html=True)
+
 
 with st.sidebar:
     st.radio(
